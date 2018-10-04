e0fc01ae251a5496f42ef4887f012eb42ca7d9ce
HieuLT22 <HieuLT22@fsoft.com.vn>
2018-10-05 03:55:38 +0700

    Create Default Website

---
+++ Shop/Areas/Centre/Controllers/DashboardController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shop.Areas.Centre.Controllers
+{
+    public class DashboardController : Controller
+    {
+        //
+        // GET: /Centre/Dashboard/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Shop/DataTransion/Response/LoginResposeDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Shop.DataTransion.Response
+{
+    public class LoginResposeDto
+    {
+    }
+}
